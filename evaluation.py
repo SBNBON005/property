be23cc099beb579899faacd3fc21aa10e2768f1a
Bongz <sibandabongz@gmail.com>
2020-06-01 16:37:28 +0200

    include bond initiation fee

--- evaluation.py
+++ evaluation.py
@@ -3,7 +3,7 @@ import math
 # Investments
 selling_price = 695985.00
 bond_interest_rate = 10.85/100
-deposit_percentage = 0
+deposit_percentage = 0/100
 monthly_effective_interest_rate = bond_interest_rate / 12
 bond_term = 30  # years
 number_of_repayments = bond_term * 12
@@ -20,13 +20,15 @@ property_management = rental_income * 10 / 100
 
 
 # Upfront Invest Amount
-down_payment = selling_price * deposit_percentage/100
+down_payment = selling_price * deposit_percentage
 asked_from_bank = selling_price - down_payment
 security_gates = 7000
 renovations = 2000  # Bathroom mirrors + soap dish
-number_of_months_without_tenant = 4
-occupation_interest = selling_price * (10/100) * number_of_months_without_tenant  # Before securing a tenant
+number_of_months_without_tenant = 5
+occupation_interest = 1/100
+occupation_rent = selling_price * occupation_interest * number_of_months_without_tenant  # Before securing a tenant
 electricity_water_meter_setup = 1500
+bond_initiation_fee = 6000
 
 
 # Analysis
@@ -34,7 +36,7 @@ monthly_repayments = asked_from_bank * (monthly_effective_interest_rate * math.p
 
 monthly_cashflow_before_tax = rental_income - monthly_repayments - levies - insurance - property_management - incase_money
 
-total_investment = down_payment + security_gates + renovations + occupation_interest + electricity_water_meter_setup
+total_investment = down_payment + security_gates + renovations + occupation_rent + electricity_water_meter_setup + bond_initiation_fee
 
 annual_cashflow = monthly_cashflow_before_tax * 12
 
